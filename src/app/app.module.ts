98ec3f5a2b093d33c6226f9d69aa44ea5e157453
mohameddhaifbelja <mohamedbeljaa@gmail.com>
2019-11-23 17:14:40 +0100

    added validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginFormComponent } from './registration/registration-form/registration-form.component';
+import { RegistrationFormComponent } from './registration/registration-form/registration-form.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    RegistrationFormComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/registration/registration-form/registration-form.component.ts
+++ src/app/registration/registration-form/registration-form.component.ts
@@ -1,25 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../../models/user';
+
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.css']
 })
-export class LoginFormComponent implements OnInit {
+export class RegistrationFormComponent implements OnInit {
   constructor() { }
    firstName = '';
    lastName = '';
    email = '';
    password = '';
-   id = '';
+   id: string ;
    phoneNumber = '';
    imageLink = '';
    birthday: Date ;
+   confirmPassword = '';
+   user: User;
 
-  onSubmit() {
-    console.log(new User(this.firstName,
-      this.lastName, this.email, this.password, this.id, this.phoneNumber, this.imageLink, this.birthday));
+  isFormValid(): boolean {
+      if (this.firstName.length < 4 ) {
+        console.log('error first name');
+        return false;
+      }
+      if (this.lastName.length <= 0 ) {
+        console.log('error last name');
+        return false;
+      }
+      if  (!this.email.match('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')) {
+        console.log('error email');
+        return false;
+      }
+      if (this.password.length < 8 || this.password !== this.confirmPassword) {
+        console.log('eror pass');
+        return false;
+      }
+      if (this.phoneNumber.toString().length !== 8) {
+        console.log('error phone number');
+        return false;
+      }
+      return true;
+   }
+
+onSubmit() {
+    if (this.isFormValid()) {
+      this.user = new User(this.firstName,
+        this.lastName, this.email, this.password, this.id, this.phoneNumber, this.imageLink, this.birthday);
+      console.log(this.user);
+    } else {
+      console.log('invalid');
+    }
   }
-  ngOnInit() {
+ngOnInit() {
   }
 }
