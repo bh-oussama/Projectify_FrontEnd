5a1d464bff424e52448f2ef334380d1aa142698a
mohamedbelja <mohamedbeljaa@gmail.com>
2020-01-14 16:01:38 +0100

    messages container and send message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RegistrationFormComponent } from './registration/registration-form/registration-form.component';
-import {FormsModule} from '@angular/forms';
-import { ChatPageComponent } from './chat/chat-page/chat-page.component';
-import { ContactListComponent } from './chat/contact-list/contact-list.component';
-import { GroupCardComponent } from './chat/group-card/group-card.component';
-import { DirectChatCardComponent } from './chat/direct-chat-card/direct-chat-card.component';
-import { MessagesListComponent } from './chat/messages-list/messages-list.component';
-import { MessageComponentComponent } from './chat/message-component/message-component.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { RegistrationFormComponent } from "./registration/registration-form/registration-form.component";
+import { FormsModule } from "@angular/forms";
+import { ChatPageComponent } from "./chat/chat-page/chat-page.component";
+import { ContactListComponent } from "./chat/contact-list/contact-list.component";
+import { GroupCardComponent } from "./chat/group-card/group-card.component";
+import { DirectChatCardComponent } from "./chat/direct-chat-card/direct-chat-card.component";
+import { MessagesListComponent } from "./chat/messages-list/messages-list.component";
+import { MessageComponentComponent } from "./chat/message-component/message-component.component";
 
 @NgModule({
   declarations: [
@@ -21,15 +21,15 @@ import { MessageComponentComponent } from './chat/message-component/message-comp
     GroupCardComponent,
     DirectChatCardComponent,
     MessagesListComponent,
-    MessageComponentComponent,
-
+    MessageComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFontAwesomeModule,
     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/chat/messages-list/messages-list.component.ts
+++ src/app/chat/messages-list/messages-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { element } from "protractor";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { Message } from "../../../models/message";
+import { DatePipe } from "@angular/common";
 
 @Component({
   selector: "app-messages-list",
@@ -7,11 +9,32 @@ import { Message } from "../../../models/message";
   styleUrls: ["./messages-list.component.css"]
 })
 export class MessagesListComponent implements OnInit {
+  messageToSend: string;
   messages: Message[];
+  dividedMessages: { date: string; messages: Message[] }[] = [];
   id: string = "1";
+  dates: string[];
   constructor() {}
+  divideMessages() {
+    var temp: { date: string; messages: Message[] }[] = [];
+    this.messages.map((m, i) => {
+      this.dates[i] = m.date;
+    });
+    this.dates = [...new Set(this.dates)];
+    this.dates.forEach((d, i) => {
+      var filtred = this.messages.filter((m, i) => {
+        return m.date == d;
+      });
+
+      temp.push({ date: d, messages: filtred });
+    });
+    this.dividedMessages = temp;
+    setTimeout(this.scrollToBottom, 100);
+  }
 
   ngOnInit() {
+    this.dividedMessages;
+    this.dates = [];
     this.messages = [
       new Message("hi man how are you doing?", "12/8/19", "2"),
       new Message("fine , thnx", "12/8/19", "1"),
@@ -24,11 +47,40 @@ export class MessagesListComponent implements OnInit {
       new Message("hi man how are you doing?", "12/8/19", "2"),
       new Message("hi man how are you doing?", "12/8/19", "1"),
       new Message("hi man how are you doing?", "12/8/19", "2"),
-      new Message("hi man how are you doing?", "12/8/19", "1"),
-      new Message("hi man how are you doing?", "12/8/19", "1"),
-      new Message("hi man how are you doing?", "12/8/19", "1"),
-      new Message("hi man how are you doing?", "12/8/19", "1"),
-      new Message("hi man how are you doing?", "12/8/19", "2")
+      new Message("hi man how are you doing?", "13/8/19", "1"),
+      new Message("hi man how are you doing?", "13/8/19", "1"),
+      new Message("hi man how are you doing?", "13/8/19", "1"),
+      new Message("hi man how are you doing?", "13/8/19", "1"),
+      new Message("hi man how are you doing?", "14/8/19", "2")
     ];
+
+    this.divideMessages();
+    console.log(this.dividedMessages);
+  }
+  scrollToBottom() {
+    var element = document.getElementById("messagescontainer");
+    element.scrollTop = element.scrollHeight;
+  }
+  ngOnChanges() {
+    this.scrollToBottom();
+  }
+
+  ngAfterViewInit() {
+    this.scrollToBottom();
+  }
+  sendMessage() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    var yy = today.getFullYear() % 100;
+    var msg = new Message(
+      this.messageToSend,
+      dd + "/" + mm + "/" + yy,
+      this.id
+    );
+    this.messages.push(msg);
+    this.divideMessages();
+    this.messageToSend = "";
+    console.log("aa");
   }
 }
